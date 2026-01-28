5241b29232f4bc6c3db8cf7cd0e31c70a8e48db5
thewarnold <ld99alt@gmail.com>
2026-01-28 08:58:26 -0600

    updated comments

--- lang705x.java
+++ lang705x.java
@@ -5,20 +5,20 @@ public class lang705x
 {
     public static void main(String[] args)throws IOException
     {
-        // 0831
-        // 08## - Row for digit
-        // ##3# - Word id in line (... ... hi <-)
-        // ###1 - where in the word the character is
+        // Initialize
         Scanner input1 = new Scanner(new File("prog705x.txt"));
         Scanner input2 = new Scanner(new File("prog512h.dat"));
         String secretword = "";
+        // Add every word to a arraylist of string arrays
         ArrayList<String[]> subwords = new ArrayList<>();
         while(input2.hasNext())
         {
             subwords.add(input2.nextLine().split(" "));
         }
+        // Run through everything
         while(input1.hasNext())
         {
+            // Get spots
             String getter = input1.next();
             String row = getter.substring(0,2);
             int realrow = Integer.parseInt(row);
@@ -26,20 +26,24 @@ public class lang705x
             int realid = Integer.parseInt(id);
             String word = getter.substring(3,4);
             int realword = Integer.parseInt(word);
+            // Decode
             String[] idk = subwords.get(realrow-1);
             String idk2 = idk[realid-1];
             String idk3 = idk2.substring(realword-1,realword);
             
             secretword=secretword+idk3;
         }
+        // Printing
         for(int x=0;x<subwords.size();x++)
         {
             for(int y=0;y<subwords.get(x).length;y++)
             {
                 System.out.print(subwords.get(x)[y]+" ");
-            }
+            } 
             System.out.println();
         }
         System.out.println("The secret word is "+secretword);
+        input1.close();
+        input2.close();
     }
 }
